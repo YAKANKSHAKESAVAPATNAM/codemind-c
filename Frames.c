a5f4daa487002955358c33355acb1e6d3d17655c
Yakanksha <147792127+YAKANKSHAKESAVAPATNAM@users.noreply.github.com>
2023-10-26 11:59:31 +0530

    #include<stdio.h> int main() {     int l,b,a;     scanf("%d%d%d",&l,&b,&a);    int perimeter=2*(l+b);     int x=a*perimeter;     printf("%d ",x); }

--- Frames.c
+++ Frames.c
@@ -3,7 +3,8 @@ int main()
 {
     int l,b,a;
     scanf("%d%d%d",&l,&b,&a);
-    int perimeter=2*(l+b);
+   int perimeter=2*(l+b);
     int x=a*perimeter;
-    printf("%d",x);
+    printf("%d
+",x);
 }
